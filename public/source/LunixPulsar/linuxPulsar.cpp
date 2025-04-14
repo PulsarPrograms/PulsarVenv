bf85b6fc902b14bedd0e01a73b249540570ee535
zeroqxq <savely-2010-mymrin@yandex.ru>
2025-04-14 21:38:36 +0300

    Create linuxPulsar.cpp

---
+++ public/source/LunixPulsar/linuxPulsar.cpp
@@ -0,0 +1,34 @@
+﻿#include "linuxPulsar.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+
+int commandAnalyze(string line){
+    if (line.empty()) {
+        return 0;
+    }
+    else if (line.starts_with("//")) {
+        return 0;
+    }
+    else if (line == "exit") {
+        return 0101;
+    }
+    else {
+         cout << "Ошибка: Команда не распознана. Введите 'help' для справки." << endl;
+    }
+}
+
+int pulsarStart(){
+    string com;
+    while (true) {
+        getline(cin, com);
+        com.erase(0, com.find_first_not_of(' '));
+        com.erase(com.find_last_not_of(' ') + 1);
+        int code = commandAnalyze(com);
+        if(code==0101){}
+    }
+    return 0;
+}
